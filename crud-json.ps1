84b964049ce86612adcf3e31b1e432e955499811
JhonatanFulguera <jfulguera@creminox.com>
2022-12-05 16:04:02 -0300

    Desarrollando index

--- crud-json.ps1
+++ crud-json.ps1
@@ -6,12 +6,13 @@ $nombreUser = [System.Environment]::UserName
 $meses = 1
 $Time = (Get-Date).AddMonths(-($meses))
 Write-Host $Time
+$fecha = "12112022"
 
-$URL = "C:\Users\$nombreUser\Documents\ImagenBackup\TEST.rar"
+$URL = "C:\Users\$nombreUser\Documents\ImagenBackup\TEST$fecha.rar"
 $result = Get-ChildItem -Path C:\Users\$nombreUser\Documents\ImagenBackup\ -Filter *.* -Recurse -ErrorAction SilentlyContinue -Force | Where-Object { $_.lastWriteTime -gt $Time }
 Write-Host $result
 
-Get-ChildItem -Path C:\Users\$nombreUser\Documents\ImagenBackup\ -Filter *.* -Recurse -ErrorAction SilentlyContinue -Force | Where-Object CreationTime -LT (Get-Date).AddDays(-10)
+#Get-ChildItem -Path C:\Users\$nombreUser\Documents\ImagenBackup\ -Filter *.* -Recurse -ErrorAction SilentlyContinue -Force | Where-Object CreationTime -LT (Get-Date).AddDays(-11)
 
 if ($result) {
         Write-Output "Listo para borrar el archivo"
@@ -19,3 +20,25 @@ if ($result) {
     }else {echo "No encontre nada"}
 
 New-Item -Path $URL
+
+#-----------------CREAR CARPETA DE USUARIO EN EL NAS------------------
+$path = "\\192.168.0.101\grupos\Sistemas\Manuales\testing\$nombreUser"
+
+if(!(test-path $path))
+{
+      New-Item -ItemType Directory -Path $path
+      Write-Output "Se creo una carpeta con el nombre del usuarioLocal"
+}
+function ConfigurarEntorno {
+    param (
+        $path
+    )
+        $path = "\\192.168.0.101\grupos\Sistemas\Manuales\testing\$nombreUser"
+
+        if(!(test-path $path)){
+            New-Item -ItemType Directory -Path $path
+            Write-Output "Se creo una carpeta con el nombre del usuarioLocal"
+        }
+}
+
+ConfigurarEntorno($path)
--- index.ps1
+++ index.ps1
@@ -1,19 +1,3 @@
-<# 
-    moverArchivo = ruta de ubicacion
-    si (Mover archivo  contiene el archibo.backup con la fecha de creacion mas reciente.) {
-        crear carpeta del usuario con el backup en caso de no tener una carpeta propia.
-        Mover archivo.iso al nas.
-        Enviar email (Mensaje de exito).
-    } si no {
-        Enviar emaail (Mensaje de error + causa);
-    }
-    
-Destino = ruta Final del archivo(server NAS)
-#>
-
-
-
-
 # ---De esta forma me ubico en la ruta en donde se encuentra la Imagen.iso
 # $rutaActual = Set-Location -Path "C:\Users\$nombreUser\Documents\ImagenBackup" -PassThru
 
@@ -21,6 +5,7 @@ Destino = ruta Final del archivo(server NAS)
 
 #-----------VARIABLES GLOBALES--------------
 $nombreUser = [System.Environment]::UserName
+
 
 #--------------Configuracion: Filtro para obtener el archivo ISO(mas reciente)------
 $dias = 2
@@ -46,15 +31,43 @@ if(!(test-path $path))
       New-Item -ItemType Directory -Path $path
       Write-Output "Se creo una carpeta con el nombre del usuarioLocal"
 }
+#Get-ChildItem -File -Recurse | Get-FileHash -Algorithm MD5 | Select-Object @{name="Name";expression={$_.Path}}, @{name="md5";expression={$_.Hash}} | ConvertTo-Json
+function GenerarJson {
+    param (
+        [string]$descripcion,[String]$estado
+    )
+    Write-Host $descripcion
+    Write-Host $estado
+    $testObject = New-Object -TypeName PSCustomObject
+    Add-Member -InputObject $testObject -MemberType NoteProperty -Name "userId" -Value 105
+    Add-Member -InputObject $testObject -MemberType NoteProperty -Name "nombre" -Value $nombreUser
+    Add-Member -InputObject $testObject -MemberType NoteProperty -Name "Mensajes" -Value $descripcion
+    Add-Member -InputObject $testObject -MemberType NoteProperty -Name "estado" -Value $estado
+
+    $jsondata = $testObject | ConvertTo-Json | Out-File "C:\Users\jfulguera\Documents\ImagenBackup\export2.json"
+
+    Write-Host $jsondata
+
+}
+
+
+
+
+
+
 
 #----------------ENVIAR IMAGEN ISO AL NAS----------------------------
 if ($result) {
     Write-Output "Proceso en marcha..."
-    Copy-Item -Path C:\Users\$nombreUser\Documents\ImagenBackup\imagenIso.txt -Destination \\192.168.0.101\grupos\Sistemas\Manuales\testing -Force
+    Copy-Item -Path C:\Users\$nombreUser\Documents\ImagenBackup\Imagen1.txt -Destination \\192.168.0.101\grupos\Sistemas\Manuales\testing\$nombreUser -Force
     
     #-----------ENVIAR CORREO 365 (CASO DE EXITO) -------------------
     #Send-MailMessage -SmtpServer smtp.office365.com -Port 587 -UseSsl -From jfulguera@creminox.com -To sistemas@creminox.com -Subject "Resultado Imagen Backup" -Body "En la maquina de $nombreUser se realizo el backup de Forma Exitosaa!😀" -Credential $credential
+    
+    $descripcion = "Tarea finalizada con Exito!"
+    $estado = "true"
 
+    GenerarJson($descripcion, $estado)
     Write-Output "Tarea finalizada con Exito!"
 }else {
     Write-Output "Proceso en marcha..."
@@ -62,21 +75,11 @@ if ($result) {
     #-----------ENVIAR CORREO 365 (CASO DE FALLO) -------------------
     #Send-MailMessage -SmtpServer smtp.office365.com -Port 587 -UseSsl -From jfulguera@creminox.com -To sistemas@creminox.com -Subject "Resultado Imagen Backup" -Body "Ocurrio un PROBLEMAA al crear la imagen en la maquina de $nombreUser !!!! 😨" -Credential $credential 
 
+    $estado = "false"
+    $descripcion = "Fallaste"
+    GenerarJson($descripcion, $estado)
     Write-Output "Error 😨😭😨"
+    
 }
 
 Write-Host $result
-
-
-
-
-
-
-<#
-$lastmonth = (get-date).AddMonths(-1).ToUniversalTime()
-
-Get-ChildItem E:\Backup |
-
-    ? { $_.CreationTime -le $lastmonth  } | 
-        Move-Item -destination F:\Backup_Process
-#> 
