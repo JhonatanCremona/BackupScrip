418f53364d835bd03fdd2c1b95214958ced47e27
JhonatanFulguera <jfulguera@creminox.com>
2022-12-07 16:39:18 -0300

    Completando la Funcion Generar Json- Armar estructura

---
+++ d
@@ -0,0 +1,29 @@
+[33mcommit 87dd6f7c82d375c5113502ca4ba63f188d75592b[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: JhonatanFulguera <jfulguera@creminox.com>
+Date:   Tue Dec 6 16:55:38 2022 -0300
+
+    Coleccion de datos en estructura Json
+
+[33mcommit 50baea5a97761ecbe1eced39b287f7f10a5dbdf7[m
+Author: JhonatanFulguera <jfulguera@creminox.com>
+Date:   Mon Dec 5 16:31:32 2022 -0300
+
+    Funcion generarJson -error de estructura con los paramatros
+
+[33mcommit d7703ba6e2feaf0fb11bd14d90fe2d5b04433ae1[m
+Author: JhonatanFulguera <jfulguera@creminox.com>
+Date:   Mon Dec 5 16:11:26 2022 -0300
+
+    Completando el Readme.md
+
+[33mcommit 84b964049ce86612adcf3e31b1e432e955499811[m
+Author: JhonatanFulguera <jfulguera@creminox.com>
+Date:   Mon Dec 5 16:04:02 2022 -0300
+
+    Desarrollando index
+
+[33mcommit b0ed503189f69807861df7a11d34dfee4c129565[m
+Author: JhonatanFulguera <jfulguera@creminox.com>
+Date:   Mon Dec 5 10:05:24 2022 -0300
+
+    Ejecutando CRUD con un Json
--- index.ps1
+++ index.ps1
@@ -1,18 +1,16 @@
 # ---De esta forma me ubico en la ruta en donde se encuentra la Imagen.iso
 # $rutaActual = Set-Location -Path "C:\Users\$nombreUser\Documents\ImagenBackup" -PassThru
 
-# ------------GUARDAMOS LA IMAGEN ISO EN EL SERVIDOR-----------
-
 #-----------VARIABLES GLOBALES--------------
 $nombreUser = [System.Environment]::UserName
-$estado = ""
 
 #--------------Configuracion: Filtro para obtener el archivo ISO(mas reciente)------
 $dias = 2
 $Time = (Get-Date).AddDays(-($dias))
+$Time
 #$result = Get-ChildItem -Path C:\Users\$nombreUser\Documents\ImagenBackup\ TEST.rar -Recurse -ErrorAction SilentlyContinue -Force | Where-Object { $.lastWriteTime -gt $Time }
 
-$result = Get-ChildItem -Path C:\Users\$nombreUser\Documents\ImagenBackup\ TEST.rar -Recurse -ErrorAction SilentlyContinue -Force
+$result = Get-ChildItem -Path C:\Users\$nombreUser\Documents\ImagenBackup\ TEST2.rar -Recurse -ErrorAction SilentlyContinue -Force
 
 #--------------Configuracion de Correo electronico-------------------
 $usuario = "jfulguera@creminox.com"
@@ -24,38 +22,39 @@ $credential = New-Object System.Management.Automation.PSCredential -ArgumentList
 
 
 #-----------------CREAR CARPETA DE USUARIO EN EL NAS------------------
-$path = "\\192.168.0.101\grupos\Sistemas\Manuales\testing\$nombreUser"
+$pathNas = "\\192.168.0.101\grupos\Sistemas\Manuales\testing\$nombreUser"
 
-if(!(test-path $path))
+if(!(test-path $pathNas))
 {
-      New-Item -ItemType Directory -Path $path
+      New-Item -ItemType Directory -Path $pathNas
       Write-Output "Se creo una carpeta con el nombre del usuarioLocal"
 }
 #Get-ChildItem -File -Recurse | Get-FileHash -Algorithm MD5 | Select-Object @{name="Name";expression={$_.Path}}, @{name="md5";expression={$_.Hash}} | ConvertTo-Json
 $URL = "C:\Users\jfulguera\Documents\visualProyects\datos1.json"
-$trans = Get-Content -Path $URL | ConvertFrom-Json
-$trans 
+$datosJson = Get-Content -Path $URL | ConvertFrom-Json
+$datosJson 
 function GenerarJson {
     param (
         
     )
     $Json =  @()
-        $Json += $trans
+        $Json += $datosJson
 
-        $info = "" | Select userId,Nombre,Resultado,estado
+        $info = "" | Select userId,Nombre,Resultado,estado,Fecha
         $info.userId = "2"
-        $info.Nombre = "Frusso"
+        $info.Nombre = $nombreUser
         $info.Resultado = $descripcion
-        $info.estado = "false"
+        $info.estado = $estado
+        $info.Fecha = Get-Date -Format "dddd MM/dd/yyyy HH:mm"
 
         $Json += $info
         $Json | ConvertTo-Json | Out-File -FilePath "C:\Users\jfulguera\Documents\visualProyects\datos1.json"
 }
-GenerarJson("")
+
 
 #----------------ENVIAR IMAGEN ISO AL NAS----------------------------
+Write-Output "Proceso en marcha..."
 if ($result) {
-    Write-Output "Proceso en marcha..."
     Copy-Item -Path C:\Users\$nombreUser\Documents\ImagenBackup\Imagen1.txt -Destination \\192.168.0.101\grupos\Sistemas\Manuales\testing\$nombreUser -Force
     
     #-----------ENVIAR CORREO 365 (CASO DE EXITO) -------------------
@@ -63,18 +62,16 @@ if ($result) {
     
     $descripcion = "Tarea finalizada con Exito!"
     $estado = "True"
-    write-host $descripcion
-    Write-Host $estado
+    GenerarJson("")
     Write-Output "Tarea finalizada con Exito!"
 }else {
-    Write-Output "Proceso en marcha..."
-
     #-----------ENVIAR CORREO 365 (CASO DE FALLO) -------------------
     #Send-MailMessage -SmtpServer smtp.office365.com -Port 587 -UseSsl -From jfulguera@creminox.com -To sistemas@creminox.com -Subject "Resultado Imagen Backup" -Body "Ocurrio un PROBLEMAA al crear la imagen en la maquina de $nombreUser !!!! 😨" -Credential $credential 
 
     $estado = "false"
-    $descripcion = "Fallaste"
-    Write-Output "Error 😨😭😨"
+    $descripcion = "No se encontro ninguna Imagen en la maquina del usuario"
+    GenerarJson("")
+    Write-Output "-----!Error--------"
     
 }
 
