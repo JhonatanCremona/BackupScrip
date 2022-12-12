deefefd8180b16e50f5d4fca97958b04d52283c4
JhonatanFulguera <jfulguera@creminox.com>
2022-12-12 10:23:11 -0300

    Crear informe del resultado -create - update -read

--- index.ps1
+++ index.ps1
@@ -4,13 +4,19 @@
 #-----------VARIABLES GLOBALES--------------
 $nombreUser = [System.Environment]::UserName
 
+$urlLocal = "C:\Users\$nombreUser\Documents\ImagenBackup\"
+$pathNas = "\\192.168.0.101\grupos\Sistemas\Manuales\testing\$nombreUser"
+$pathJson = "C:\Users\jfulguera\Documents\visualProyects\datos1.json"
+
+
+
 #--------------Configuracion: Filtro para obtener el archivo ISO(mas reciente)------
 $dias = 2
 $Time = (Get-Date).AddDays(-($dias))
 $Time
 #$result = Get-ChildItem -Path C:\Users\$nombreUser\Documents\ImagenBackup\ TEST.rar -Recurse -ErrorAction SilentlyContinue -Force | Where-Object { $.lastWriteTime -gt $Time }
 
-$result = Get-ChildItem -Path C:\Users\$nombreUser\Documents\ImagenBackup\ TEST2.rar -Recurse -ErrorAction SilentlyContinue -Force
+$result = Get-ChildItem -Path $urlLocal TEST2.rar -Recurse -ErrorAction SilentlyContinue -Force
 
 #--------------Configuracion de Correo electronico-------------------
 $usuario = "jfulguera@creminox.com"
@@ -18,11 +24,7 @@ $pass = "Cremona.2022"
 [SecureString]$securepassword = $pass | ConvertTo-SecureString -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList $usuario, $securepassword
 
-
-
-
 #-----------------CREAR CARPETA DE USUARIO EN EL NAS------------------
-$pathNas = "\\192.168.0.101\grupos\Sistemas\Manuales\testing\$nombreUser"
 
 if(!(test-path $pathNas))
 {
@@ -30,39 +32,54 @@ if(!(test-path $pathNas))
       Write-Output "Se creo una carpeta con el nombre del usuarioLocal"
 }
 #Get-ChildItem -File -Recurse | Get-FileHash -Algorithm MD5 | Select-Object @{name="Name";expression={$_.Path}}, @{name="md5";expression={$_.Hash}} | ConvertTo-Json
-$URL = "C:\Users\jfulguera\Documents\visualProyects\datos1.json"
-$datosJson = Get-Content -Path $URL | ConvertFrom-Json
-$datosJson 
+
+$datosJson = Get-Content -Path $pathJson | ConvertFrom-Json
+$datosJson[0].Nombre.Contains("jfulguera") 
 function GenerarJson {
     param (
         
     )
-    $Json =  @()
+    $cantidad = 0
+    for ($i = 0; $i -lt $datosJson.values.Count; $i++) {
+        $dato = $datosJson[$i].Nombre.Contains($nombreUser)
+        if (!$dato) {
+            $cantidad = $cantidad + 1;
+        }
+    }
+    if ($cantidad -eq $datosJson.values.Count) {
+        Write-Output "No existe este usuario"
+        $Json =  @()
         $Json += $datosJson
 
         $info = "" | Select userId,Nombre,Resultado,estado,Fecha
-        $info.userId = "2"
+        $info.userId = $datosJson.values.Count + 1
         $info.Nombre = $nombreUser
         $info.Resultado = $descripcion
         $info.estado = $estado
         $info.Fecha = Get-Date -Format "dddd MM/dd/yyyy HH:mm"
 
         $Json += $info
-        $Json | ConvertTo-Json | Out-File -FilePath "C:\Users\jfulguera\Documents\visualProyects\datos1.json"
+        $Json | ConvertTo-Json | Out-File -FilePath $pathJson
+
+    } else {
+        Write-Output "Existe este usuario"
+    }
+    
 }
 
 
 #----------------ENVIAR IMAGEN ISO AL NAS----------------------------
 Write-Output "Proceso en marcha..."
 if ($result) {
-    Copy-Item -Path C:\Users\$nombreUser\Documents\ImagenBackup\Imagen1.txt -Destination \\192.168.0.101\grupos\Sistemas\Manuales\testing\$nombreUser -Force
-    
+    Copy-Item -Path $urlLocal\testing.js -Destination $pathNas -Force
+
     #-----------ENVIAR CORREO 365 (CASO DE EXITO) -------------------
     #Send-MailMessage -SmtpServer smtp.office365.com -Port 587 -UseSsl -From jfulguera@creminox.com -To sistemas@creminox.com -Subject "Resultado Imagen Backup" -Body "En la maquina de $nombreUser se realizo el backup de Forma Exitosaa!😀" -Credential $credential
     
     $descripcion = "Tarea finalizada con Exito!"
     $estado = "True"
     GenerarJson("")
+
     Write-Output "Tarea finalizada con Exito!"
 }else {
     #-----------ENVIAR CORREO 365 (CASO DE FALLO) -------------------
@@ -71,8 +88,15 @@ if ($result) {
     $estado = "false"
     $descripcion = "No se encontro ninguna Imagen en la maquina del usuario"
     GenerarJson("")
+
     Write-Output "-----!Error--------"
     
 }
 
 Write-Host $result
+
+
+<#
+    BORRAR Archivos del NAS -
+    Crear Carpetas con fechas - que contengan el 
+#>
