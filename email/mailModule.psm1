29d57136f6dcbb091140ed2e0180eb0d2b0722fe
Jhonatan Fulguera <jfulguera@CREMONA.local>
2022-12-26 15:02:54 -0300

    mailModule Completo

--- email/mailModule.psm1
+++ email/mailModule.psm1
@@ -13,9 +13,66 @@ function Send-MailKitMessage {
         [Parameter()]
         $BCC,
         [Parameter()]
-        
-
-
+        [String]$Subject="",
+        [Parameter()]
+        [String]$Body = "",
+        [Parameter()]
+        $Attachments,
+        [Parameter(Mandatory)]
+        [String]$SMPTServer,
+        [Parameter()]
+        [int32]$Port = 25,
+        [Parameter()]
+        [switch]$BodyAsHtml,
+        [Parameter()]
+        $credenciales
     )
-    
+    $SMTP=New-Object MailKit.Net.Smtp.SmtpClient
+    $Message=New-Object MimeKit.MimeMessage
+    $Builder=New-Object MimeKit.BodyBuilder
+
+    $Message.From.Add($From)
+
+    foreach($Person in $To){
+        $Message.To.Add($Person)
+    }
+
+    if($CC){
+        foreach($Person in $CC){
+            $Message.Cc.Add($Person)
+        }
+    }
+
+    if($BCC){
+        foreach($Person in $BCC){
+            $Message.Bcc.Add($Person)
+        }
+    }
+
+    $Message.Subject=$Subject
+
+    if($BodyAsHtml){
+        $Builder.HtmlBody=$Body
+    }else{
+        $Builder.TextBody=$Body
+    }
+
+    if($Attachments){
+        foreach($Attachment in $Attachments){
+            $Builder.Attachments.Add($Attachment)
+        }
+    }
+
+    $Message.Body=$Builder.ToMessageBody()
+
+    $SMTP.Connect($SMTPServer,$Port,$false)
+
+    if($Credential){
+        $SMTP.Authenticate($Credential.username,$Credential.getNetworkCredential().password)
+    }
+
+    $SMTP.Send($Message)
+
+    $SMTP.Disconnect($true)
+    $SMTP.Dispose()
 }
