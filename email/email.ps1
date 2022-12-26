de55c737946ed8ac419d54d9c785a933882bae29
Jhonatan Fulguera <jfulguera@CREMONA.local>
2022-12-26 14:13:28 -0300

    Enviar mensaje-->Completo

--- email/email.ps1
+++ email/email.ps1
@@ -1,20 +1,21 @@
 Install-Package -Name 'MimeKit' -Source "https://www.nuget.org/api/v2" -SkipDependencies
 Install-Package -Name 'MailKit' -Source "https://www.nuget.org/api/v2"
 
-Add-Type -Path "C:\Program Files\PackageManagement\NuGet\Packages\MailKit.3.4.3\lib\netstandard2.0\MailKit.dll"
+#----------------Este modulo solo funciona si se ejecuta con PówerShell 7-----------------------------
 Add-Type -Path "C:\Program Files\PackageManagement\NuGet\Packages\MimeKit.3.4.3\lib\netstandard2.0\MimeKit.dll"
+Add-Type -Path "C:\Program Files\PackageManagement\NuGet\Packages\MailKit.3.4.3\lib\netstandard2.0\MailKit.dll"
+
 
 #-----------------Con este comando encriptamos la contraseña del usuario--------------
 #Get-Credential | Export-Clixml -Path C:\Users\jhona\OneDrive\Documentos\ProyectShell\BackupScrip\email\email.xml
-
+#Get-Credential | Export-Clixml -Path C:\Users\jfulguera\Desktop\ProyectScript\BackupScrip\email\emailtest.xml
 
 $SMTP = New-Object MailKit.Net.Smtp.SmtpClient
 $Message = New-Object MimeKit.MimeMessage
 $Builder = New-Object MimeKit.BodyBuilder
 
-$Account = Import-Clixml -Path C:\Users\jhona\OneDrive\Documentos\ProyectShell\BackupScrip\email\email.xml
-$MyCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Account.UserName $Account.Password
- 
+#$Account = Import-Clixml -Path C:\Users\jhona\OneDrive\Documentos\ProyectShell\BackupScrip\email\email.xml
+$Account = Import-Clixml -Path C:\Users\jfulguera\Desktop\ProyectScript\BackupScrip\email\emailtest.xml
 
 $Message.From.add("jfulguera@creminox.com")
 $Message.to.Add("jhonatanful@outlook.es")
@@ -23,3 +24,6 @@ $Builder.TextBody = "Mensaje de prueba"
 $Message.Body = $builder.ToMessageBody()
 $SMTP.Connect("smtp.office365.com",587,$false)
 $SMTP.Authenticate($Account)
+$SMTP.Send($Message)
+$SMTP.Disconnect($true)
+$SMTP.Dispose()
